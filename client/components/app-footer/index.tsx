3efe376ae9b38158f46065cbcc0bb6b9fe4bc107
Michael North <michael.l.north@gmail.com>
2017-09-18 10:03:36 -0700

    footer

--- client/components/app-footer/index.tsx
+++ client/components/app-footer/index.tsx
@@ -5,7 +5,7 @@ import './styles.scss';
 const AppFooter = () => (
   <footer className='footer'>
     <div className='mui-container-fluid'>
-      🙌 I built this in a <a href='https://frontendmasters.com' rel='noopener noreferrer' target='_blank'>Frontendmasters</a> Visual Studio Code course.
+      🙌 I built this in a <a href='http://go/training-schedule' rel='noopener noreferrer' target='_blank'>LinkedIn</a> Visual Studio Code course.
     </div>
   </footer>
 );
